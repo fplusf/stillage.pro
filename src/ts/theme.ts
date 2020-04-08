7b6901428b1b4e5dd7ed053a9c568b0ff1b1b076
Farrukh <fuzailof@gmail.com>
2020-04-08 21:19:48 +0500

    Header > searchbox open/close implemented.

--- src/ts/theme.ts
+++ src/ts/theme.ts
@@ -1,2 +1,14 @@
 
-const search = document.getElementById('')
+const searchForm = document.getElementById('searchForm'),
+        searchBtn = document.querySelector('.top-bar__search-btn'),
+        searchClose = document.querySelector('.search-box__close');
+
+searchBtn.addEventListener("click", function() {
+    searchForm.style.width = '100%';
+    searchForm.style.display = 'flex';
+})
+
+searchClose.addEventListener("click", () => {
+    searchForm.style.width = '0';
+    searchForm.style.display = 'none';
+})

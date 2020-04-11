ff6fae630f74eb80669578f5a8e5e64d3924a2ed
Farrukh <fuzailof@gmail.com>
2020-04-11 13:46:47 +0500

    Header > Search box & Cart issues fixed.

--- src/ts/theme.ts
+++ src/ts/theme.ts
@@ -1,14 +1,17 @@
 const searchForm = document.getElementById('searchForm'),
+    searchInput = document.getElementById('search-input'),
     searchBtn = document.querySelector('.top-bar__search-btn'),
     searchClose = document.querySelector('.search-box__close'),
     cartButton = document.querySelector('.top-bar__cart_button'),
-    cartWrapper = document.querySelector('.cart-wrapper');
+    cartWrapper = document.querySelector('.cart-wrapper'),
+    cartOverlay = document.querySelector('.cart-overlay');
 
 /**
  * Manipulate search box.
  */
 searchBtn.addEventListener('click', function () {
     searchForm.classList.add('top-bar__search-box_show');
+    searchInput.focus()
 });
 
 searchClose.addEventListener('click', () => {
@@ -20,4 +23,11 @@ searchClose.addEventListener('click', () => {
  */
 cartButton.addEventListener('click', () => {
     cartWrapper.classList.toggle('cart-wrapper_visible');
+    cartOverlay.classList.toggle('show-cart-overlay');
+    searchForm.classList.remove('top-bar__search-box_show');
+});
+
+cartOverlay.addEventListener('click', () => {
+    cartWrapper.classList.remove('cart-wrapper_visible');
+    cartOverlay.classList.remove('show-cart-overlay');
 });

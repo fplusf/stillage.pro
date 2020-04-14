a5fe04c33f1b4c33131e6dfea04d505cfb62c73d
Farrukh <fuzailof@gmail.com>
2020-04-14 22:00:27 +0500

    Carousel implemented.

--- gulpfile.js
+++ gulpfile.js
@@ -119,6 +119,7 @@ function serve() {
 
 function watchALL() {
     watch('src/sass/**/*.sass', compileSASS);
+    watch('src/*.sass', compileSASS);
     watch('src/img/**/*.png');
     watch('src/components/**/*.sass', compileSASS);
     watch('src/ts/**/*.ts', transpileTS);
